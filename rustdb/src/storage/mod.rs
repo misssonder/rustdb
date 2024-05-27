053ed349d7bb8f2138b039e91ea5ab6651c834ff
misssonder <819643718@qq.com>
2024-05-27 14:31:38 +0800

    Update table structure and add table node

--- rustdb/src/storage/mod.rs
+++ rustdb/src/storage/mod.rs
@@ -9,6 +9,7 @@ pub type PageId = usize;
 
 pub type AtomicPageId = AtomicUsize;
 pub const NULL_PAGE: PageId = PageId::MAX;
+pub type TableId = u16;
 
 #[derive(Debug, Copy, Clone, PartialOrd, PartialEq)]
 pub struct RecordId {
