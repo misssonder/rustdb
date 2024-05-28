f9016ca69cdc08c5ca3cbbc4448881ea216ba56e
misssonder <819643718@qq.com>
2024-05-28 23:05:23 +0800

    Add engine trait

--- rustdb/src/storage/mod.rs
+++ rustdb/src/storage/mod.rs
@@ -1,3 +1,7 @@
+use crate::error::{RustDBError, RustDBResult};
+use crate::sql::catalog::{TableId, TableRefId};
+use crate::storage::page::column::Column;
+use std::future::Future;
 use std::sync::atomic::AtomicUsize;
 
 pub mod disk;
@@ -15,4 +19,18 @@ pub const NULL_PAGE: PageId = PageId::MAX;
 pub struct RecordId {
     pub page_id: PageId,
     pub slot_num: u32,
+}
+
+pub trait Engine {
+    type Table;
+    fn create_table(
+        &self,
+        id: TableRefId,
+        name: &str,
+        columns: &[Column],
+    ) -> impl Future<Output = RustDBResult<()>> + Send;
+
+    fn read_table(&self, id: TableRefId) -> impl Future<Output = RustDBResult<Self::Table>>;
+
+    fn delete_tale(&self, id: TableRefId) -> impl Future<Output = RustDBResult<()>>;
 }
